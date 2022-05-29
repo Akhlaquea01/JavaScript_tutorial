7cc04da944a4563c3518a391ca5229de80c27253
Akhlaque <akhlaquea01@gmail.com>
2022-05-29 17:13:53 +0530

    Day 1: Exercises

---
+++ variable.js
@@ -0,0 +1,20 @@
+// Declaring different variables of different data types
+// Declare variables to store your first name, last name, marital status, country and age in multiple lines
+
+let firstName = 'Akhlaque'; // first name of a person
+let lastName = 'Ahmad'; // last name of a person
+let country = 'India'; // country
+let city = 'Muzaffarpur'; // city
+let age = 23; // age in years
+let isMarried = false;
+let nullValue = null;
+let nickName
+
+// Declare variables to store your first name, last name, marital status, country and age in a single line
+
+let firstName2 = 'Akhlaque'; let lastName2 = 'Ahmad'; let country2 = 'India'; let city2 = 'Muzaffarpur'; let age2 = 23; let isMarried2 = false;
+
+const myAge = 23;
+const yourAge = 32;
+console.log(`I am ${myAge} years old.`);
+console.log(`You are ${yourAge} years old.`);
