7ac8ab674ae4dd1075f25b75b11714005f0d96b3
Akhlaque <akhlaquea01@gmail.com>
2023-03-10 14:20:45 +0530

    Array

--- 009-array.js
+++ 009-array.js
@@ -1,8 +1,4 @@
-///////////////////////////////////////
-// Introduction to Arrays
-const friend1 = 'Michael';
-const friend2 = 'Steven';
-const friend3 = 'Peter';
+// TOPIC: Introduction to Arrays
 
 const friends = [ 'Michael', 'Steven', 'Peter' ];
 console.log(friends);
@@ -10,162 +6,136 @@ console.log(friends);
 const y = new Array(1991, 1984, 2008, 2020);
 
 console.log(friends[ 0 ]);
-console.log(friends[ 2 ]);
 
 console.log(friends.length);
 console.log(friends[ friends.length - 1 ]);
 
 friends[ 2 ] = 'Jay';
 console.log(friends);
-// friends = ['Bob', 'Alice']
 
 const firstName = 'Jonas';
 const jonas = [ firstName, 'Schmedtmann', 2037 - 1991, 'teacher', friends ];
 console.log(jonas);
 console.log(jonas.length);
 
-// Exercise
+
 const calcAge = function (birthYeah) {
-    return 2037 - birthYeah;
+  return new Date().getFullYear() - birthYeah;
 };
 const years = [ 1990, 1967, 2002, 2010, 2018 ];
-
-const age1 = calcAge(years[ 0 ]);
-const age2 = calcAge(years[ 1 ]);
-const age3 = calcAge(years[ years.length - 1 ]);
-console.log(age1, age2, age3);
 
 const ages = [ calcAge(years[ 0 ]), calcAge(years[ 1 ]), calcAge(years[ years.length - 1 ]) ];
 console.log(ages);
 
 
-///////////////////////////////////////
-// Basic Array Operations (Methods)
-const friends = [ 'Michael', 'Steven', 'Peter' ];
+// TOPIC: Basic Array Operations (Methods)
+const friendsArray = [ 'Michael', 'Steven', 'Peter' ];
 
-// Add elements
-const newLength = friends.push('Jay');
-console.log(friends);
+// => Add elements
+const newLength = friendsArray.push('Jay'); //INFO: will return new length after pushing
+console.log(friendsArray);
 console.log(newLength);
 
-friends.unshift('John');
-console.log(friends);
+friendsArray.unshift('John');
+console.log(friendsArray);
 
-// Remove elements
-friends.pop(); // Last
-const popped = friends.pop();
+// => Remove elements
+friendsArray.pop(); // Last
+const popped = friendsArray.pop();
 console.log(popped);
-console.log(friends);
+console.log(friendsArray);
 
-friends.shift(); // First
-console.log(friends);
+friendsArray.shift(); // First
+console.log(friendsArray);
 
-console.log(friends.indexOf('Steven'));
-console.log(friends.indexOf('Bob'));
+// => IndexOf
+console.log(friendsArray.indexOf('Steven'));
+console.log(friendsArray.indexOf('Bob'));
 
-friends.push(23);
-console.log(friends.includes('Steven'));
-console.log(friends.includes('Bob'));
-console.log(friends.includes(23));
+// => Includes
+friendsArray.push(23);
+console.log(friendsArray.includes('Steven'));
+console.log(friendsArray.includes('Bob'));
+console.log(friendsArray.includes(23));
 
-if (friends.includes('Steven')) {
-    console.log('You have a friend called Steven');
+if (friendsArray.includes('Steven')) {
+  console.log('You have a friend called Steven');
 }
 
 
-///////////////////////////////////////
-// Coding Challenge #2
 
-/*
-Steven is still building his tip calculator, using the same rules as before: Tip 15% of the bill if the bill value is between 50 and 300, and if the value is different, the tip is 20%.
 
-1. Write a function 'calcTip' that takes any bill value as an input and returns the corresponding tip, calculated based on the rules above (you can check out the code from first tip calculator challenge if you need to). Use the function type you like the most. Test the function using a bill value of 100.
-2. And now let's use arrays! So create an array 'bills' containing the test data below.
-3. Create an array 'tips' containing the tip value for each bill, calculated from the function you created before.
-4. BONUS: Create an array 'total' containing the total values, so the bill + tip.
+let arr = [ 'a', 'b', 'c', 'd', 'e' ];
 
-TEST DATA: 125, 555 and 44
+// TOPIC: SLICE
 
-HINT: Remember that an array needs a value in each position, and that value can actually be the returned value of a function! So you can just call a function as array values (so don't store the tip values in separate variables first, but right in the new array) 😉
+// => The slice() method returns selected elements in an array, as a new array.
+// => The slice() method selects from a given start, up to a (not inclusive) given end.
+// => The slice() method does not change the original array.
 
-GOOD LUCK 😀
-*/
-
-const calcTip = function (bill) {
-    return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
-};
-// const calcTip = bill => bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
-
-const bills = [ 125, 555, 44 ];
-const tips = [ calcTip(bills[ 0 ]), calcTip(bills[ 1 ]), calcTip(bills[ 2 ]) ];
-const totals = [ bills[ 0 ] + tips[ 0 ], bills[ 1 ] + tips[ 1 ], bills[ 2 ] + tips[ 2 ] ];
-
-console.log(bills, tips, totals);
-
-/////////////////////////////////////////////////
-// Simple Array Methods
-let arr = ['a', 'b', 'c', 'd', 'e'];
-
-// SLICE
 console.log(arr.slice(2));
 console.log(arr.slice(2, 4));
 console.log(arr.slice(-2));
 console.log(arr.slice(-1));
 console.log(arr.slice(1, -2));
 console.log(arr.slice());
-console.log([...arr]);
+console.log([ ...arr ]);
 
-// SPLICE
-// console.log(arr.splice(2));
-arr.splice(-1);
-console.log(arr);
-arr.splice(1, 2);
-console.log(arr);
+// TOPIC: SPLICE
 
-// REVERSE
-arr = ['a', 'b', 'c', 'd', 'e'];
-const arr2 = ['j', 'i', 'h', 'g', 'f'];
-console.log(arr2.reverse());
-console.log(arr2);
+// =>The splice() method adds and/or removes array elements.
+// =>The splice() method overwrites the original array.
+// => 3 onwards New elements(s) to be added.
 
-// CONCAT
-const letters = arr.concat(arr2);
+let arrSplice = [ 'a', 'b', 'c', 'd', 'e' ];
+console.log(arrSplice.splice(2)); //['c', 'd', 'e']
+arrSplice.splice(-1);
+console.log(arrSplice); //['a']
+arrSplice.splice(1, 2, '1', '2', '3'); // ['a', '1', '2', '3']
+console.log(arrSplice);
+
+// TOPIC: REVERSE
+
+// => The reverse() method reverses the order of the elements in an array.
+// => The reverse() method overwrites the original array.
+
+const arrRevrse = [ 'j', 'i', 'h', 'g', 'f' ];
+console.log(arrRevrse.reverse());
+
+// TOPIC: CONCAT
+
+// => The concat() method concatenates (joins) two or more arrays.
+// => The concat() method returns a new array, containing the joined arrays.
+// => The concat() method does not change the existing arrays.
+
+const letters = arr.concat(arrRevrse);
 console.log(letters);
-console.log([...arr, ...arr2]);
+console.log([ ...arr, ...arrRevrse ]);
 
-// JOIN
-console.log(letters.join(' - '));
+// TOPIC: JOIN
+
+// => The join() method returns an array as a string.
+// => The join() method does not change the original array.
+// => Any separator can be specified. The default is comma (,).
+console.log(letters.join(' -&- '));
 
 
-///////////////////////////////////////
-// The new at Method
-const arr3 = [23, 11, 64];
-console.log(arr3[0]);
-console.log(arr3.at(0));
+// TOPIC: at Method
 
-// getting last array element
-console.log(arr3[arr3.length - 1]);
-console.log(arr3.slice(-1)[0]);
-console.log(arr3.at(-1));
-
+// => The at() method takes an integer value and returns the item at that index.
+console.log(arr[ 0 ]);
+console.log(arr.at(0));
 console.log('jonas'.at(0));
+
+console.log(arr[ arr.length - 1 ]);
+console.log(arr.at(-1));
 console.log('jonas'.at(-1));
 
 
-///////////////////////////////////////
-// Looping Arrays: forEach
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
-// for (const movement of movements) {
-for (const [i, movement] of movements.entries()) {
-  if (movement > 0) {
-    console.log(`Movement ${i + 1}: You deposited ${movement}`);
-  } else {
-    console.log(`Movement ${i + 1}: You withdrew ${Math.abs(movement)}`);
-  }
-}
+// TOPIC: Looping Arrays: forEach
 
-console.log('---- FOREACH ----');
+const movements = [ 200, 450, -400, 3000, -650, -130, 70, 1300 ];
 movements.forEach(function (mov, i, arr) {
   if (mov > 0) {
     console.log(`Movement ${i + 1}: You deposited ${mov}`);
@@ -174,175 +144,128 @@ movements.forEach(function (mov, i, arr) {
   }
 });
 
-///////////////////////////////////////
-// The filter Method
+// TOPIC: The filter Method
 const deposit = movements.filter(function (mov, i, arr) {
-    return mov > 0;
-  });
-  console.log(movements);
-  console.log(deposit);
-  
-  const depositsFor = [];
-  for (const mov of movements) if (mov > 0) depositsFor.push(mov);
-  console.log(depositsFor);
-  
-  const withdrawal = movements.filter(mov => mov < 0);
-  console.log(withdrawal);
-  
-  
-  ///////////////////////////////////////
-  // The reduce Method
-  console.log(movements);
-  
-  // accumulator -> SNOWBALL
-  // const balance = movements.reduce(function (acc, cur, i, arr) {
-  //   console.log(`Iteration ${i}: ${acc}`);
-  //   return acc + cur;
-  // }, 0);
-  const balance = movements.reduce((acc, cur) => acc + cur, 0);
-  console.log(balance);
-  
-  let balance2 = 0;
-  for (const mov of movements) balance2 += mov;
-  console.log(balance2);
-  
-  // Maximum value
-  const max = movements.reduce((acc, mov) => {
-    if (acc > mov) return acc;
-    else return mov;
-  }, movements[0]);
-  console.log(max);
-
-  ///////////////////////////////////////
-// The Magic of Chaining Methods
-const eurToUsd = 1.1;
+  return mov > 0;
+});
 console.log(movements);
+console.log(deposit);
 
-// PIPELINE
-const totalDepositsUSD = movements
-  .filter(mov => mov > 0)
-  .map((mov, i, arr) => {
-    // console.log(arr);
-    return mov * eurToUsd;
-  })
-  // .map(mov => mov * eurToUsd)
-  .reduce((acc, mov) => acc + mov, 0);
+const depositsFor = [];
+for (const mov of movements) if (mov > 0) depositsFor.push(mov);
+console.log(depositsFor);
+
+const withdrawal = movements.filter(mov => mov < 0);
+console.log(withdrawal);
+
+
+// TOPIC: The reduce Method
+
+const balance = movements.reduce((acc, cur) => acc + cur, 0);
+console.log(balance);
+
+let balance2 = 0;
+for (const mov of movements) balance2 += mov;
+console.log(balance2);
+
+// Maximum value
+const max = movements.reduce((acc, mov) => {
+  if (acc > mov) return acc;
+  else return mov;
+}, movements[ 0 ]);
+console.log(max);
+
+// TOPIC: The Magic of Chaining Methods
+const eurToUsd = 1.1;
+
+const totalDepositsUSD = movements.filter(mov => mov > 0).map((mov, i, arr) => {
+  return mov * eurToUsd;
+}).reduce((acc, mov) => acc + mov, 0);
 console.log(totalDepositsUSD);
 
-///////////////////////////////////////
-// The find Method
+// TOPIC: Find Method
 const firstWithdrawal = movements.find(mov => mov < 0);
 console.log(movements);
 console.log(firstWithdrawal);
 
-console.log(accounts);
 
-const account = accounts.find(acc => acc.owner === 'Jessica Davis');
-console.log(account);
-
-
-///////////////////////////////////////
-// some and every
-console.log(movements);
-
-// EQUALITY
-console.log(movements.includes(-130));
-
-// SOME: CONDITION
+// TOPIC: SOME
 console.log(movements.some(mov => mov === -130));
 
 const anyDeposits = movements.some(mov => mov > 0);
 console.log(anyDeposits);
 
-// EVERY
+// TOPIC: EVERY
 console.log(movements.every(mov => mov > 0));
-console.log(account4.movements.every(mov => mov > 0));
 
-// Separate callback
+// TOPIC: Separate callback
 const deposit2 = mov => mov > 0;
 console.log(movements.some(deposit2));
 console.log(movements.every(deposit2));
 console.log(movements.filter(deposit2));
 
-///////////////////////////////////////
-// flat and flatMap
-const arr4 = [[1, 2, 3], [4, 5, 6], 7, 8];
+// TOPIC: Flat
+
+// => A new array with the sub-array elements concatenated into it.
+// =>The depth level specifying how deep a nested array structure should be flattened. Defaults to 1.
+
+const arr4 = [ [ 1, 2, 3 ], [ 4, 5, 6 ], 7, 8 ];
 console.log(arr4.flat());
 
-const arrDeep = [[[1, 2], 3], [4, [5, 6]], 7, 8];
+const arrDeep = [ [ [ 1, 2 ], 3 ], [ 4, [ 5, 6 ] ], 7, 8 ];
 console.log(arrDeep.flat(2));
 
-// flat
-const overalBalance = accounts
-  .map(acc => acc.movements)
-  .flat()
-  .reduce((acc, mov) => acc + mov, 0);
-console.log(overalBalance);
 
-// flatMap
-const overalBalance2 = accounts
-  .flatMap(acc => acc.movements)
-  .reduce((acc, mov) => acc + mov, 0);
-console.log(overalBalance2);
+// TOPIC: FlatMap
 
-///////////////////////////////////////
-// Sorting Arrays
+// => The flatMap() method returns a new array formed by applying a given callback function to each element of the array, and then flattening the result by one level.
 
-// Strings
-const owners = ['Jonas', 'Zach', 'Adam', 'Martha'];
+const arr1 = [ 1, 2, [ 3 ], [ 4, 5 ], 6, [] ];
+const flattened = arr1.flatMap(num => num);
+console.log(flattened); // Expected output: Array [1, 2, 3, 4, 5, 6]
+
+
+const arr2 = [ "it's Sunny in", "", "California" ];
+arr1.map((x) => x.split(" ")); // [["it's","Sunny","in"],[""],["California"]]
+arr1.flatMap((x) => x.split(" ")); // ["it's","Sunny","in", "", "California"]
+
+
+// TOPIC: Sorting Arrays
+
+// => Strings
+const owners = [ 'Jonas', 'Zach', 'Adam', 'Martha' ];
 console.log(owners.sort());
-console.log(owners);
 
-// Numbers
-console.log(movements);
-
-// return < 0, A, B (keep order)
-// return > 0, B, A (switch order)
+// => Numbers
 
 // Ascending
-// movements.sort((a, b) => {
-//   if (a > b) return 1;
-//   if (a < b) return -1;
-// });
 movements.sort((a, b) => a - b);
 console.log(movements);
 
 // Descending
-// movements.sort((a, b) => {
-//   if (a > b) return -1;
-//   if (a < b) return 1;
-// });
 movements.sort((a, b) => b - a);
 console.log(movements);
 
-// More Ways of Creating and Filling Arrays
-const arr5 = [1, 2, 3, 4, 5, 6, 7];
-console.log(new Array(1, 2, 3, 4, 5, 6, 7));
 
-// Emprty arrays + fill method
+// TOPIC: Fill method
+
+// => fill(value, start, end)
+
 const x = new Array(7);
 console.log(x);
-// console.log(x.map(() => 5));
 x.fill(1, 3, 5);
 x.fill(1);
 console.log(x);
+x.fill(23, 2, 6);
+console.log(x);
 
-arr.fill(23, 2, 6);
-console.log(arr5);
-
-// Array.from
+// TOPIC: Array.from
 const y2 = Array.from({ length: 7 }, () => 1);
-console.log(y2);
+console.log(y2); //[1, 1, 1, 1, 1, 1, 1]
 
 const z = Array.from({ length: 7 }, (_, i) => i + 1);
-console.log(z);
+console.log(z); // [1, 2, 3, 4, 5, 6, 7]
 
-labelBalance.addEventListener('click', function () {
-  const movementsUI = Array.from(
-    document.querySelectorAll('.movements__value'),
-    el => Number(el.textContent.replace('€', ''))
-  );
-  console.log(movementsUI);
+console.log(Array.from('foo')); // Expected output: Array ["f", "o", "o"]
 
-  const movementsUI2 = [...document.querySelectorAll('.movements__value')];
-});
+console.log(Array.from([ 1, 2, 3 ], x => x + x)); // Expected output: Array [2, 4, 6]
--- 011-forLoop.js
+++ 011-forLoop.js
@@ -30,3 +30,13 @@ for (let i = 0; i < jonas.length; i++) {
 
     console.log(jonas[ i ], typeof jonas[ i ]);
 }
+
+const movements = [ 200, 450, -400, 3000, -650, -130, 70, 1300 ];
+
+for (const [ i, movement ] of movements.entries()) {
+  if (movement > 0) {
+    console.log(`Movement ${i + 1}: You deposited ${movement}`);
+  } else {
+    console.log(`Movement ${i + 1}: You withdrew ${Math.abs(movement)}`);
+  }
+}
