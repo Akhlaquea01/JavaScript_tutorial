0d0a620114316702a4c7059d5ac6e7dc898fe2a5
Akhlaque <akhlaquea01@gmail.com>
2023-03-11 12:09:27 +0530

    Object fix

--- 010-object.js
+++ 010-object.js
@@ -1,189 +1,91 @@
-// Dot vs. Bracket Notation
-const jonas = {
-    firstName: 'Jonas',
-    lastName: 'Schmedtmann',
-    age: 2037 - 1991,
-    job: 'teacher',
-    friends: [ 'Michael', 'Peter', 'Steven' ]
+// TOPIC: Dot vs. Bracket Notation
+const atts = {
+    firstName: 'Akhlaque',
+    lastName: 'Ahmad',
+    age: new Date().getFullYear() - 1999,
+    job: 'coder',
+    friends: [ 'Aman', 'Firoz', 'Choti' ]
 };
-console.log(jonas);
+console.log(atts);
 
-console.log(jonas.lastName);
-console.log(jonas[ 'lastName' ]);
+console.log(atts.firstName);
+console.log(atts[ 'lastName' ]);
 
 const nameKey = 'Name';
-console.log(jonas[ 'first' + nameKey ]);
-console.log(jonas[ 'last' + nameKey ]);
+console.log(atts[ 'first' + nameKey ]);
+console.log(atts[ 'last' + nameKey ]);
 
-// console.log(jonas.'last' + nameKey)
+const interestedIn = "firstName";
 
-const interestedIn = prompt('What do you want to know about Jonas? Choose between firstName, lastName, age, job, and friends');
-
-if (jonas[ interestedIn ]) {
-    console.log(jonas[ interestedIn ]);
+if (atts[ interestedIn ]) {
+    console.log(atts[ interestedIn ]);
 } else {
     console.log('Wrong request! Choose between firstName, lastName, age, job, and friends');
 }
 
-jonas.location = 'Portugal';
-jonas[ 'twitter' ] = '@jonasschmedtman';
-console.log(jonas);
+atts.location = 'Portugal';
+atts[ 'twitter' ] = '@iam_atts_';
+console.log(atts);
 
-// Challenge
-// "Jonas has 3 friends, and his best friend is called Michael"
-console.log(`${jonas.firstName} has ${jonas.friends.length} friends, and his best friend is called ${jonas.friends[ 0 ]}`);
+console.log(`${atts.firstName} has ${atts.friends.length} friends, and his best friend is called ${atts.friends[ 0 ]}`);
 
 
-///////////////////////////////////////
-// Object Methods
+// TOPIC: Object Methods
 
-const jonas = {
-    firstName: 'Jonas',
-    lastName: 'Schmedtmann',
-    birthYeah: 1991,
-    job: 'teacher',
-    friends: [ 'Michael', 'Peter', 'Steven' ],
+const myData = {
+    firstName: 'Akhlaque',
+    lastName: 'Ahmad',
+    birthYeah: 1999,
+    job: 'coder',
+    friends: [ 'Aman', 'Firoz', 'Varun' ],
     hasDriversLicense: true,
 
     calcAge: function () {
-        this.age = 2037 - this.birthYeah;
+        this.age = new Date().getFullYear() - this.birthYeah;
         return this.age;
     },
 
     getSummary: function () {
-        return `${this.firstName} is a ${this.calcAge()}-year old ${jonas.job}, and he has ${this.hasDriversLicense ? 'a' : 'no'} driver's license.`;
+        return `${this.firstName} is a ${this.calcAge()}-year old ${myData.job}, and he has ${this.hasDriversLicense ? 'a' : 'no'} driver's license.`;
     }
 };
+console.log(myData.calcAge());
+console.log(myData.age);
+console.log(myData.getSummary());
 
-console.log(jonas.calcAge());
 
-console.log(jonas.age);
 
-console.log(jonas.getSummary());
+// TOPIC: Looping Objects: Object Keys, Values, and Entries
 
-///////////////////////////////////////
-// Coding Challenge #3
-
-/*
-Let's go back to Mark and John comparing their BMIs! This time, let's use objects to implement the calculations! Remember: BMI = mass / height ** 2 = mass / (height * height). (mass in kg and height in meter)
-
-1. For each of them, create an object with properties for their full name, mass, and height (Mark Miller and John Smith)
-2. Create a 'calcBMI' method on each object to calculate the BMI (the same method on both objects). Store the BMI value to a property, and also return it from the method.
-3. Log to the console who has the higher BMI, together with the full name and the respective BMI. Example: "John Smith's BMI (28.3) is higher than Mark Miller's (23.9)!"
-
-TEST DATA: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 m tall.
-
-GOOD LUCK 😀
-*/
-
-const mark = {
-    fullName: 'Mark Miller',
-    mass: 78,
-    height: 1.69,
-    calcBMI: function () {
-        this.bmi = this.mass / this.height ** 2;
-        return this.bmi;
-    }
+const openingHours = {
+    'sun': { open: '10', close: '18' },
+    'mon': { open: '9', close: '19' },
+    'tue': { open: '9', close: '19' },
+    'wed': { open: '9', close: '19' },
+    'thu': { open: '9', close: '19' },
+    'fri': { open: '9', close: '21' },
+    'sat': { open: '10', close: '18' },
 };
 
-const john = {
-    fullName: 'John Smith',
-    mass: 92,
-    height: 1.95,
-    calcBMI: function () {
-        this.bmi = this.mass / this.height ** 2;
-        return this.bmi;
-    }
-};
-
-mark.calcBMI();
-john.calcBMI();
-
-console.log(mark.bmi, john.bmi);
-
-// "John Smith's BMI (28.3) is higher than Mark Miller's (23.9)!"
-
-if (mark.bmi > john.bmi) {
-    console.log(`${mark.fullName}'s BMI (${mark.bmi}) is higher than ${john.fullName}'s BMI (${john.bmi})`);
-} else if (john.bmi > mark.bmi) {
-    console.log(`${john.fullName}'s BMI (${john.bmi}) is higher than ${mark.fullName}'s BMI (${mark.bmi})`);
-}
-
-///////////////////////////////////////
-// Coding Challenge #3
-
-/* 
-Let's continue with our football betting app! This time, we have a map with a log of the events that happened during the game. The values are the events themselves, and the keys are the minutes in which each event happened (a football game has 90 minutes plus some extra time).
-
-1. Create an array 'events' of the different game events that happened (no duplicates)
-2. After the game has finished, is was found that the yellow card from minute 64 was unfair. So remove this event from the game events log.
-3. Print the following string to the console: "An event happened, on average, every 9 minutes" (keep in mind that a game has 90 minutes)
-4. Loop over the events and log them to the console, marking whether it's in the first half or second half (after 45 min) of the game, like this:
-      [FIRST HALF] 17: ⚽️ GOAL
-
-GOOD LUCK 😀
-*/
-
-const gameEvents = new Map([
-    [17, '⚽️ GOAL'],
-    [36, '🔁 Substitution'],
-    [47, '⚽️ GOAL'],
-    [61, '🔁 Substitution'],
-    [64, '🔶 Yellow card'],
-    [69, '🔴 Red card'],
-    [70, '🔁 Substitution'],
-    [72, '🔁 Substitution'],
-    [76, '⚽️ GOAL'],
-    [80, '⚽️ GOAL'],
-    [92, '🔶 Yellow card'],
-  ]);
-  
-  
-  // 1.
-  const events = [...new Set(gameEvents.values())];
-  console.log(events);
-  
-  // 2.
-  gameEvents.delete(64);
-  
-  // 3.
-  console.log(
-    `An event happened, on average, every ${90 / gameEvents.size} minutes`
-  );
-  const time = [...gameEvents.keys()].pop();
-  console.log(time);
-  console.log(
-    `An event happened, on average, every ${time / gameEvents.size} minutes`
-  );
-  
-  // 4.
-  for (const [min, event] of gameEvents) {
-    const half = min <= 45 ? 'FIRST' : 'SECOND';
-    console.log(`[${half} HALF] ${min}: ${event}`);
-  }
-
-  ///////////////////////////////////////
-// Looping Objects: Object Keys, Values, and Entries
-
-// Property NAMES
+// => Property NAMES
 const properties = Object.keys(openingHours);
-console.log(properties);
+console.log(properties); //[ 'a', 'b', 'c','d', 'e', 'f', 'g']
 
 let openStr = `We are open on ${properties.length} days: `;
 for (const day of properties) {
-  openStr += `${day}, `;
+    openStr += `${day}, `;
 }
 console.log(openStr);
 
-// Property VALUES
+// => Property VALUES
 const values = Object.values(openingHours);
 console.log(values);
 
-// Entire object
+// => Entire object
 const entries = Object.entries(openingHours);
-// console.log(entries);
+console.log(entries);
 
-// [key, value]
-for (const [day, { open, close }] of entries) {
-  console.log(`On ${day} we open at ${open} and close at ${close}`);
+// => [key, value]
+for (const [ day, { open, close } ] of entries) {
+    console.log(`On ${day} we open at ${open} and close at ${close}`);
 }
