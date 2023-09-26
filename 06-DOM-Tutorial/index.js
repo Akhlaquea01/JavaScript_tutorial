648ffaba956063a72d88f54258f33842eb9bced7
Akhlaque <akhlaquea01@gmail.com>
2023-09-27 00:22:12 +0530

    minor Update

--- 06-DOM-Tutorial/index.js
+++ 06-DOM-Tutorial/index.js
@@ -18,3 +18,64 @@ user.addEventListener('click', () => { console.log("I run only once"); }, { once
 function get() {
     console.dirxml(document.querySelector(".h"));
 }
+
+// Section 2: Selecting Elements
+const elementById = document.getElementById("elementById");
+const elementByName = document.getElementsByName("elementByName");
+const elementsByTagName = document.getElementsByTagName("p");
+const elementsByClassName = document.getElementsByClassName("example-class");
+const selectedByCSS = document.querySelector(".example-class");
+
+// Section 3: Traversing Elements
+const parentElement = document.getElementById("parentElement");
+const childElements = parentElement.children;
+
+// Section 4: Manipulating Elements
+document.getElementById("createElementButton").addEventListener("click", () => {
+    const newElement = document.createElement("p");
+    newElement.textContent = "Newly Created Element";
+    document.getElementById("appendChildExample").appendChild(newElement);
+});
+
+const textContentExample = document.getElementById("textContentExample");
+textContentExample.addEventListener("click", () => {
+    textContentExample.textContent = "Text content changed!";
+});
+
+const innerHTMLExample = document.getElementById("innerHTMLExample");
+innerHTMLExample.addEventListener("click", () => {
+    innerHTMLExample.innerHTML = "<em>HTML content changed!</em>";
+});
+
+// Section 5: Working with Attributes
+const attributeExample = document.getElementById("attributeExample");
+attributeExample.setAttribute("data-custom-attribute", "456");
+const customAttribute = attributeExample.getAttribute("data-custom-attribute");
+
+// Section 6: Manipulating Element's Styles
+const styleExample = document.getElementById("styleExample");
+styleExample.style.color = "red";
+
+// Section 7: Working with Events
+const eventButton = document.getElementById("eventButton");
+const eventLog = document.getElementById("eventLog");
+
+eventButton.addEventListener("click", () => {
+    eventLog.textContent = "Button clicked!";
+});
+
+// Section 8: Scripting Web Forms
+const exampleForm = document.getElementById("exampleForm");
+exampleForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const textInputValue = document.getElementById("textInput").value;
+    const radioValue = document.querySelector('input[name="radioExample"]:checked').value;
+    const checkboxValue = document.getElementById("checkboxExample").checked;
+    const selectValue = document.getElementById("selectBox").value;
+
+    console.log("Form submitted with values:");
+    console.log("Text Input: " + textInputValue);
+    console.log("Radio Value: " + radioValue);
+    console.log("Checkbox Value: " + checkboxValue);
+    console.log("Select Value: " + selectValue);
+});
