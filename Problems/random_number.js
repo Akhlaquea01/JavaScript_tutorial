37e3668370c430ee2fcc552f68b3a35448209fe4
Akhlaque <akhlaquea01@gmail.com>
2024-04-21 00:56:20 +0530

    type conversion

---
+++ Problems/random_number.js
@@ -0,0 +1,4 @@
+// Generating random number between a range using Math.random()
+const min = 10;
+const max = 20;
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
--- Topics/004-type-conversion.js
+++ Topics/004-type-conversion.js
@@ -1,18 +1,28 @@
 // TOPIC: Type Conversion
+
+// Converting string to number using Number constructor
 const inputYear = '1991';
 console.log(Number(inputYear), inputYear);
 console.log(Number(inputYear) + 18);
 
+// Converting non-numeric string to NaN
 console.log(Number('Jonas'));
 console.log(typeof NaN);
 
+// Converting number to string using String constructor
 console.log(String(23), 23);
 
+
 // TOPIC: Type Coercion
+
+// Implicit type coercion: Number to string concatenation
 console.log('I am ' + 23 + ' years old');
+
+// Implicit type coercion: String to number subtraction and division
 console.log('23' - '10' - 3);
 console.log('23' / '2');
 
+// Explicit type coercion: String to number and then back to string
 let n = '1' + 1; // '11'
 n = n - 1;
 console.log(n);
@@ -20,28 +30,9 @@ console.log(n);
 
 // TOPIC: Truthy and Falsy Values
 
-// => 5 falsy values: 0, '', undefined, null, NaN
-console.log(Boolean(0));
-console.log(Boolean(undefined));
-console.log(Boolean('Jonas'));
-console.log(Boolean({}));
-console.log(Boolean(''));
-
-const money = 100;
-if (money) {
-  console.log("Don't spend it all ;)");
-} else {
-  console.log('You should get a job!');
-}
-
-let height = 0;
-if (height) {
-  console.log('YAY! Height is defined');
-} else {
-  console.log('Height is UNDEFINED');
-}
-
-// INFO: To get random number between max and min
-const min = 10
-const max = 20
-console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+// Falsy values: 0, '', undefined, null, NaN
+console.log(Boolean(0)); // false
+console.log(Boolean(undefined)); // false
+console.log(Boolean('Jonas')); // true
+console.log(Boolean({})); // true
+console.log(Boolean('')); // false
