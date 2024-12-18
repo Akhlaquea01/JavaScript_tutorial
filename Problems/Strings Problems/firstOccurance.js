b6e9bf4e3b0c075631a4793d8fcdd8a6e3285dca
Akhlaque <akhlaquea01@gmail.com>
2024-12-18 23:00:09 +0530

    2nd method

--- Problems/Strings Problems/firstOccurance.js
+++ Problems/Strings Problems/firstOccurance.js
@@ -4,4 +4,17 @@ const strStr = function (haystack, needle) {
     return haystack.indexOf(needle);
 };
 
-console.log(strStr('Akhlaque','aq'));
+console.log(strStr('Akhlaque', 'aq'));
+
+function strStr2(haystack, needle) {
+    const needleLength = needle.length;
+    const haystackLength = haystack.length;
+
+    for (let i = 0; i <= haystackLength - needleLength; i++) {
+        if (haystack.substring(i, i + needleLength) === needle) {
+            return i; // Return the index if a match is found
+        }
+    }
+    return -1; // Return -1 if no match is found
+}
+console.log(strStr2('Akhlaque', 'aq'));
