ab197ba6b09f6034f626b9591acdef33cfce3720
Akhlaque <akhlaquea01@gmail.com>
2025-03-26 17:57:48 +0530

    Add function to calculate the sum of all even numbers in an array

---
+++ Problems/Loops Problems/sumOfEvent.js
@@ -0,0 +1,15 @@
+/**
+ * write a function to find the sum of all even numbers in that array
+ */
+
+function sumOfEvent(arr) {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] % 2 === 0) {
+            sum += arr[i];
+        }
+    }
+    return sum;
+}
+
+console.log(sumOfEvent([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
